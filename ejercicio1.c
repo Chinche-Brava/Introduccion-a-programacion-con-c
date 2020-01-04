40e185b99471ce5dcff018f8fadd680dafb39d6d
Alan Rodríguez <alan.rodgar8@gmail.com>
2020-01-03 21:48:50 -0600

    nuevo proyecto final >:v

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-//escribe en c
- int main() {
-   //primero declaro las variables
-  int a;
-  float b;
-  //luego las defino
-  a = 13 % 2;
-  b = 2.0/(1.0+2-(a+1));
-  //Al final imprimo su valor
-  printf("El valor de a es: %d y el valor de b es: %f \n",a,b);
-
-  return 0;
-}
+jejeje :<
